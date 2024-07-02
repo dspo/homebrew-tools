c4eb50462e896423d52c3cb8168d3598e6a95074
悟空 <rainchan365@163.com>
2024-07-02 13:49:53 +0800

    feat: update git-todo.rb

--- Formula/git-todo.rb
+++ Formula/git-todo.rb
@@ -4,14 +4,27 @@
 class GitTodo < Formula
   desc "git todo extension"
   homepage "https://github.com/dspo/git-todo"
-  url "https://github.com/dspo/homebrew-git-todo/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-  license ""
-  version "0.0.1"
+  url "https://github.com/dspo/git-todo/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "4942295c1b167b3e14f09df29bb296df825f9b84f3fac1372fd20d23a583cfe1"
+  license "MIT"
+
   depends_on "rust" => :build
 
   def install
     system "cargo", "build", "--release", "--bin", "git-todo"
     bin.install "target/release/git-todo"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test git-todo`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
   end
 end
